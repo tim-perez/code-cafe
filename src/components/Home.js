fd9a37bb349470eb95df0699552c0aef89ea00f5
Tim Perez <timperez624@gmail.com>
2024-07-01 23:31:29 -0700

    update home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ function Home({ items }) {
       {items.map((item) => (
         <Thumbnail
           key={item.itemId}
+          itemId={item.itemId}
           image={itemImages[item.imageId]}
           title={item.title}
         />
