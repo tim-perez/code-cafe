99b80168aebbf93f1c06883abed318715bc177f2
Tim Perez <timperez624@gmail.com>
2024-07-02 00:04:49 -0700

    installing a server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const server = express();
+const port = 9000;
+
+const apiProxy = createProxyMiddleware({
+  target: 'http://localhost:3030/api',
+  changeOrigin: true,
+});
+
+server.use('/api', apiProxy);
+
+server.use(express.static(path.join(__dirname, 'build')));
+
+server.get('/hello', (req, res) => {
+  res.send('Hello World');
+});
+
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
